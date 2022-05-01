19250dceb3235611d3e57aeb5ec6b6548c6ff884
Hiszpan_ <wojtek.szacon@gmail.com>
2022-05-01 22:40:14 +0200

    Stworzenie podstawowego szablonu strony

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const http = require('http');
+const path = require('path');
 const express = require('express')
 const fetch = require('node-fetch');
 const app = express()
 
 var { Liquid } = require('liquidjs');
-var engine = new Liquid();
+var engine = new Liquid({
+  root: path.resolve(__dirname, 'views/'),  // root for layouts/includes lookup
+  extname: '.liquid'          // used for layouts/includes, defaults ""
+});
+// register liquid engine
+app.engine('liquid', engine.express()); 
+app.set('views', './views');            // specify the views directory
+app.set('view engine', 'liquid'); 
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+
 
 function searchAnime(searchQuery) {
   let max_search_results = 10;
@@ -28,7 +39,6 @@ function searchAnime(searchQuery) {
 
 app.get('/', function (req, res) {
   res.send('Hello World');
-  
 })
 
 app.get('/searchAnime', function (req, res) {
@@ -38,23 +48,20 @@ app.get('/searchAnime', function (req, res) {
     let api_request_response = searchAnime(req.searchQuery);
     //sprawdzamy czy zmienna jest tablicą, jeśli ją nie jest to sprawdzamy czy jest stringiem bo jeśli jest to wystąpił błąd
     if(Array.isArray(api_request_response)) {
-
-    } else if ("abc") {
+      res.send('jeszcze tutaj trzeba coś zrobić');
+    } /*else if ("abc") {
       //API coś się wtedy zrąbało
       console.log("abc");
 
     } else {
       //nieznany błąd
       console.log("")
-    }
+    }*/
 
   } else {
     //generowanie strony jak ktoś chce wyszukać sobie przy pomocy tej strony (bo do tej strony można przekierować będzie ze strony głównej bo tam zrobię jakiś )
+    res.render('db_default_view', {subsite_title: 'Szukaj tytułu', paragraph_content: '(tutaj będzie duże pole wyszukiwania)'});
   }
-
-  
-  res.send('Hello World');
-  
 })
 
 app.listen(port)
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+Poor MAL Version™
+
 Używane pakiety z npm:
 - [express](https://www.npmjs.com/package/express)
 - [node-fetch (w wersji drugiej)](https://www.npmjs.com/package/node-fetch)
